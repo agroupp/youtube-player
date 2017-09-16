560418a85ef59ff9c1cf519105f96661a13b72e0
Arthur Groupp <arth.groupp@gmail.com>
2017-09-16 20:15:23 +0300

    Added conditional redirect to ssl site

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+function isInDev(): boolean {
+  return (process && process.env && process.env.NODE_ENV === 'development');
+}
+if (!isInDev() && location.protocol === 'http:') {
+  location.assign(`https://${location.host}`);
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
